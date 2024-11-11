7dae2d911001c77971d3a299d946cf9971bf2e10
gabi09 <luizrafael_20@hotmail.com>
2024-11-11 11:46:21 -0300

    JavaS

---
+++ materia-javascript/js/funcoes.js
@@ -0,0 +1,45 @@
+// console.log("teste")
+// let variavel = 1
+// console.log(variavel)
+
+// function executar() {
+// console.log("executar 1")
+// console.log("executar 1")
+// console.log("executar 1")
+// console.log("executar 1")
+// }
+
+// // executar()
+
+// function somar(x, y){
+// console.log(x + y)
+// }
+
+// let numero_1 = 40
+// let numero_2 = 240
+// let numero_3 = 100
+// somar(numero_1, numero_2)
+// somar(numero_2, numero_3)
+// console.log("\n")
+
+// function subtrair(x, y){
+// console.log(x - y)
+// // console.log("\n")
+// }
+
+// let number_1 = 100
+// let number_2 = 200
+// let number_3 = 50
+
+// subtrair(number_2, number_1)
+// subtrair(number_1, number_3)
+
+// console.log("\n")
+
+function media(a, b){
+    let resultado = ((a + b) / 2)
+
+    console.log("a média é: "  + resultado ) 
+}
+    media(8,7)
+
